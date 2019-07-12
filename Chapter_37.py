0a999842c1ef89e23fce1990c6b7ec70b5a88cae
Roman Carey (Mac) <roman192376544@mail.ru>
2019-07-12 22:33:18 +0300

    temp

--- Chapter_37.py
+++ Chapter_37.py
@@ -380,10 +380,43 @@ del X.attr2
 
 ##################################################
 class Person:
-    def __init__(self, name):
-        self._name = name
-    
+    def __init__(self, name):  # Called when creating a Person()
+        self._name = name      # Will call __setattr__ !!!
+#    def __getattr__(self, attr):   # Called by obj.udefined
+#        if attr == 'name':         # for an absent attribute with the name 'name'
+#            print('fetch...')      # 'name' is managed attribute
+#            return self._name  # Does not loop: existing attribute
+#        else:
+#            raise AttributeError(attr)  # Turning to the other non-existent 
+#                                        # attributes will call an error
+    def __getattribute__(self, attr): # Called by obj.any -
+        if attr == 'name':            # - intercepts calls to any names
+            print('fetch...')
+            attr = '_name'   # Maps to the internal name '_name'
+        return object.__getattribute__(self, attr)  # To prevent looping 
+    def __setattr__(self, attr, value): # Called by obj.any = value
+        if attr == 'name':       # 'name' is managed attribute
+            print('change...')
+            attr = '_name'     # The internal name of the attribute
+        self.__dict__[attr] = value   # To prevent looping
+    def __delattr__(self, attr):   # Called by del obj.name
+        if attr == 'name':       # 'name' is managed attribute
+            print('remove...')
+            attr = '_name'
+        del self.__dict__[attr]  # To prevent looping
 
+if __name__ == '__main__':
+    bob = Person('Bob Smith')  # 'name' is managed attribute
+    print(bob.name)            # Will call __getattr__
+    bob.name = 'Robert Smith'  # Will call __setattr__
+    print(bob.name)
+    del bob.name               # Will call __delattr__
+    print('-' * 20)
+    sue = Person('Sue Jones')    
+    print(sue.name)
+
+
+##################################################
 
 
 
