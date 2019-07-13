dbdd9e085b68871b5036528c88066f1e37f8ef87
Roman Carey (Mac) <roman192376544@mail.ru>
2019-07-14 00:07:55 +0300

    Chapter #37 -  modified again

--- Chapter_37.py
+++ Chapter_37.py
@@ -515,14 +515,103 @@ print(X.attr3)
 
             
 ##################################################
-        
+class Powers:
+    def __init__(self, square, cube):        
+        self._square = square
+        self._cube = cube
+    def getSquare(self):
+        return self._square ** 2
+    def setSquare(self, value):
+        self._square = value
+    square = property(getSquare, setSquare)
+    def getCube(self):
+        return self._cube ** 3
+    cube = property(getCube)
+
+X = Powers(3, 4)
+print(X.square)
+print(X.cube)
+X.square = 5
+print(X.square)
 
 
+##################################################
+class DescSquare:
+    def __get__(self, instance, owner):
+        return instance._square ** 2
+    def __set__(self, instance, value):
+        instance._square = value
+
+class DescCube:
+    def __get__(self, instance, owner):
+        return instance._cube ** 3
+
+class Powers:
+    square = DescSquare()
+    cube = DescCube()
+    def __init__(self, square, cube):        
+        self._square = square   # 'self.square = square' will also work - 
+        self._cube = cube       # - because it will call 'DescSquare.__set__'
+
+X = Powers(3, 4)
+print(X.square)
+print(X.cube)
+X.square = 5
+print(X.square)
 
 
+##################################################
+class Powers:
+    def __init__(self, square, cube):        
+        self._square = square
+        self._cube = cube
+    def __getattr__(self, name):
+        if name == 'square':
+            return self._square ** 2
+        elif name == 'cube':
+            return self._cube ** 3
+        else:
+            raise AttributeError('unknown attribute: ' + name)
+    def __setattr__(self, name, value):
+        if name == 'square':
+            name = '_square'
+#        object.__setattr__(self, name, value)
+        self.__dict__[name] = value
+
+X = Powers(3, 4)
+print(X.square)
+print(X.cube)
+X.square = 5
+print(X.square)
 
 
+##################################################
+class Powers:
+    def __init__(self, square, cube):        
+        self._square = square
+        self._cube = cube
+    def __getattribute__(self, name):
+        if name == 'square':
+            return object.__getattribute__(self, '_square') ** 2
+        elif name == 'cube':
+            return object.__getattribute__(self, '_cube') ** 3
+        else:
+            return object.__getattribute__(self, name)
+    def __setattr__(self, name, value):
+        if name == 'square':
+            name = '_square'
+#        object.__setattr__(self, name, value)
+        self.__dict__[name] = value
 
+X = Powers(3, 4)
+print(X.square)
+print(X.cube)
+X.square = 5
+print(X.square)
+
+
+##################################################
+       
 
 
 
