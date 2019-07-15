4639646af29a9eb681a58ba59e1b39ba1ef842a2
romchegue <borodinrs1@gmail.com>
2019-07-15 11:10:39 +0300

    Chapter_37.py modified

--- Chapter_37.py
+++ Chapter_37.py
@@ -666,10 +666,40 @@ for Class in GetAttr, GetAttribute:
 
 
 ##################################################
+class Person:
+    def __init__(self, name, job=None, pay=0):
+        self.name = name
+        self.job = job
+        self.pay = pay
+    def lastName(self):
+        return self.name.split()[-1]
+    def giveRaise(self, percent):
+        self.pay = int(self.pay * (1 + percent))
+    def __str__(self):
+        return '[Person: {0}, {1}]'.format(self.name, self.pay)
+
+class Manager:
+    def __init__(self, name, pay):
+        self.person = Person(name, 'mgr', pay)
+    def giveRaise(self, percent, bonus=0.10):
+        self.person.giveRaise(percent + bonus)
+    def __getattr__(self, attr):
+        return getattr(self.person, attr)
+    def __str__(self):               # Must redefine in Python 3.x 
+        return str(self.person)
+
+if __name__ == '__main__':
+    sue = Person('Sue Jones', job='dev', pay=100000)
+    print(sue.lastName())
+    sue.giveRaise(.10)
+    print(sue)
+    tom = Manager('Tom Hanks', 50000)   # Manager.__init__
+    print(tom.lastName())               # Manager.__getattr__ ---> Person.lastName
+    tom.giveRaise(.10)                  # Manager.giveRaise ---> Person.giveRaise
+    print(tom)                          # Manager.__str__ ---> Person.__str__
 
 
-
-
+##################################################
 
 
 
