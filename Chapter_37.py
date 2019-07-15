eda021dca0979a2b570a26a6c5ba1d0a9db43213
romchegue <borodinrs1@gmail.com>
2019-07-15 09:56:13 +0300

    _

--- Chapter_37.py
+++ Chapter_37.py
@@ -611,7 +611,61 @@ print(X.square)
 
 
 ##################################################
-       
+# file: getattr.py 
+
+class GetAttr:
+    eggs = 88
+    def __init__(self):
+        self.spam = 77
+    def __len__(self):
+        print('__len__: 42')	
+        return 42
+    def __getattr__(self, attr):
+        print('getattr: ' + attr)
+        if attr == '__str__':
+            return lambda *args: '[Getattr str]'
+        else:
+            return lambda *args: None
+
+class GetAttribute:
+    eggs = 88
+    def __init__(self):
+        self.spam = 77
+    def __len__(self):
+        print('__len__: 42')	
+        return 42
+    def __getattribute__(self, attr):		
+        print('getattribute: ' + attr)
+        if attr == '__str__':
+            return lambda *args: '[GetAttribute str]'
+        else:
+            return lambda *args: None
+
+for Class in GetAttr, GetAttribute:
+    print('\n' + Class.__name__.ljust(50, '='))
+	X = Class()
+	X.eggs
+	X.spam
+	X.other
+	len(X)
+	try:
+	    X[0]
+    except:
+        print('fail []')
+	try:
+	    X + 99
+    except:
+        print('fail +')    
+	try:
+	    X()
+    except:
+        print('fail ()')
+    X.__call__()
+    print(X.__str__())
+    print(X)
+
+
+##################################################
 
 
 
@@ -627,36 +681,3 @@ print(X.square)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<<<<<<< HEAD
-
-
-
-=======
-
-
-
-
-
-
-
-
-
-
-
->>>>>>> dbdd9e085b68871b5036528c88066f1e37f8ef87
